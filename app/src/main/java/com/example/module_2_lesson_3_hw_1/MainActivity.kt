1b751ed41a69e81f1ae06906abd73b10a780af9b
VD <vdfree448@protonmail.com>
2022-07-02 11:36:18 +0300

    Remove Action Bar. Changed theme in file AndroidManifest.xml. android:theme="@style/Theme.Module_2_Lesson_3_hw_1"  ->    Theme.Design.Light.NoActionBar

--- app/src/main/java/com/example/module_2_lesson_3_hw_1/MainActivity.kt
+++ app/src/main/java/com/example/module_2_lesson_3_hw_1/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.module_2_lesson_3_hw_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//remove UP BAR
+//        requestWindowFeature(Window.FEATURE_NO_TITLE)
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+//remove TITLE BAR
+//        supportActionBar?.hide()
+
         setContentView(R.layout.activity_main)
+
+
+
+
+
     }
 }
